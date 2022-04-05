0592b6abc626df7a844d966f923838957e819079
Mnishek <shavyrina.marina@mail.ru>
2022-04-05 22:12:26 +0300

    files from lecture

---
+++ src/dbapi/modules/comments/comments.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from '@nestjs/common';
+import { CommentDTO } from '../../dto/comment.dto';
+import { PostsService } from '../posts/posts.service';
+import * as fs from 'fs';
+import { Response } from 'express';
+import { MyLogger } from '../logger/logger.service';
+import { MailService } from '../../../mail/mail.service';
+import { Comment } from '../../database/entities/comment.entity';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+
+// let commentId = 3;
+@Injectable()
+export class CommentsService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentsRepository: Repository<Comment>,
+    private readonly postsService: PostsService,
+    private readonly mailService: MailService,
+    private myLogger: MyLogger,
+  ) {
+    this.myLogger.setContext('CommentsService');
+  }
+
+  async getComments(id: number): Promise<CommentDTO[]> {
+    const posts = await this.postsService.getPosts();
+    return posts[id].comments;
+  }
+
+  async getComment(postId: number, commentId: number): Promise<CommentDTO> {
+    const posts = await this.postsService.getPosts();
+    return posts[postId].comments[commentId];
+  }
+
+  async createComment(postId: number, data: CommentDTO): Promise<CommentDTO> {
+    await this.mailService.sendMessage('testmail188@mail.ru');
+    const posts = await this.postsService.getPosts()
+    
+    return this.commentsRepository.save({
+      postId: postId,
+      ...data
+    });
+  }
+
+  // async createComment(postId: number, data: CommentDTO): Promise<CommentDTO> {
+  //   const comment: CommentDTO = {
+  //     ...data,
+  //     id: commentId++,
+  //     createdAt: new Date(Date.now())
+
+  //   }
+
+  //   await this.mailService.sendMessage('testmail188@mail.ru')
+  //   const posts = await this.postsService.getPosts()
+
+  //   posts[postId].comments.push(comment);
+
+  //   return data;
+
+  // }
+
+  
+  async deleteComment(postId: number, commentId: number): Promise<Comment> {
+
+    const posts = await this.postsService.getPosts()
+    const post = posts[postId-1]
+    const comment = post.comments[commentId-1]
+    const comments = post.comments
+    // const index = comments.findIndex(item => item.id == commentId)
+    if (comment) {
+      return this.commentsRepository.remove(comment);
+    } else throw new Error('Comment not found');
+  }
+
+  // async deleteComment(postId: number, commentId: number): Promise<PostsDTO[]> {
+
+  //   const posts = await this.postsService.getPosts()
+  //   const post = posts[postId - 1]
+  //   console.log(posts[postId - 1]);
+
+  //   const comments = post.comments
+  //   const index = comments.findIndex(item => item.id == commentId)
+  //   if (index >= 0) {
+  //     comments.splice(index, 1);
+  //     return posts;
+  //   } else throw new Error('Comment not found');
+  // }
+
+  async updateComment(postId: number, commentId: number, data: CommentDTO): Promise<CommentDTO> {
+    const posts = await this.postsService.getPosts()
+    const post = posts[postId - 1]
+    const comments = post.comments
+    const index = comments.findIndex(item => item.id == commentId)
+    const commentLast = comments[index].text
+    let existingComment = comments[index];
+    existingComment = {
+      ...existingComment,
+      ...data,
+    };
+    comments[commentId - 1] = existingComment;
+    const messageUpdate = { 'last': commentLast, 'now': comments[commentId - 1].text }
+    await this.mailService.sendMessageUpdateComment('testmail188@mail.ru', messageUpdate)
+    return comments[commentId - 1];
+
+  }
+
+  async saveFile(path: string, data: Buffer) {
+    fs.writeFile(path, data, (error) => {
+      if (error) throw new Error(error.message);
+    });
+  }
+
+  async getFile(response: Response) {
+    const buffer = fs.createReadStream('/Users/user/blog/files/receipt.pdf');
+    this.myLogger.warn('About to return cats!');
+    buffer.pipe(response).on('close', () => {
+      buffer.destroy();
+    });
+  }
+}
